ee0cf3af46b1d139e73a01aaf2c89f00a409aba6
Mathias Bast <mathiasbast123@gmail.com>
2020-06-17 12:29:43 +1200

    tries something else

--- server/routes.js
+++ server/routes.js
@@ -1,37 +1,24 @@
 require('dotenv').config()
 const express = require('express')
 const router = express.Router()
-const jwt = require('jsonwebtoken')
+const passport = require('passport')
 
 const utils = require('./utils')
 
-router.get('/', authenticateToken, (req, res) => {
+router.get('/', (req, res) => {
   utils.getData('data', (err, data) => {
     if (err) return res.status(500).send(err.message)
     res.status(200).json(data)
   })
 })
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(err)
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
+router.post('/login',
+  passport.authenticate('local'),
+  function (req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username)
   })
-}
-
-router.post('/login', (req, res) => {
-  const username = req.body.username
-  const user = { name: username }
-
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-  res.json({ accessToken })
-})
 
 router.post('/:username/:password', (req, res) => {
   const { username, password } = req.params
