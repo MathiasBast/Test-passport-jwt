444db08181dfa59b183f662ecd5bec639102761e
Mathias Bast <mathiasbast123@gmail.com>
2020-06-18 12:11:25 +1200

    add passport middlewear

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const passport = require('passport')
 
 const server = express()
 
@@ -7,6 +8,7 @@ const user = require('./routes')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+server.use(passport.initialize())
 
 server.use('/api/v1/user', user)
 
