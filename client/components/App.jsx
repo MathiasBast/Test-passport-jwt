c8108122d4c415831a906a0ad9543fda83d23b14
Mathias Bast <mathiasbast123@gmail.com>
2020-07-01 16:42:36 +1200

    adds login page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom'
 
 import Form from './Form'
 import See from './See'
+import LogIn from './LogIn'
 
 const App = () => {
   return (
@@ -12,7 +13,11 @@ const App = () => {
       <Link to='/register'>
         <button>register</button>
       </Link>
+      <Link to='/login'>
+        <button>Log In</button>
+      </Link>
       <Route exact path='/register' component={Form} />
+      <Route exact path='/login' component={LogIn} />
       <See />
     </>
   )
